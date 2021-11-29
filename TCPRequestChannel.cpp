797ec869eb5d50080547001ba99e4e8261506655
Joshua Kim <joshuakim135@gmail.com>
2021-11-28 23:36:45 -0500

    finished functionality of given functions

--- TCPRequestChannel.cpp
+++ TCPRequestChannel.cpp
@@ -3,23 +3,28 @@
 void TCPRequestChannel::serverSetUp(const string port) {
 
 }
-void TCPRequestChannel::clientSetUp(const string host, const string port) {
 
+void TCPRequestChannel::clientSetUp(const string host, const string port) {
+    
 }
 
 TCPRequestChannel::TCPRequestChannel (const string host_name, const string port_no) {
     // if host name empty string -> server side; else -> client side
     (host_name.empty()) ? serverSetUp(port_no) : clientSetUp(host_name, port_no);
 }
-TCPRequestChannel::TCPRequestChannel(int) {
 
+TCPRequestChannel::TCPRequestChannel(int s) {
+    sockfd = s;
 }
+
 TCPRequestChannel::~TCPRequestChannel() {
-    
+    close(sockfd);
 }
+
 int TCPRequestChannel::cread(void* msgbuf, int buflen) {
-
+    return read(sockfd, msgbuf, buflen);
 }
-int TCPRequestChannel::cwrite(void* msgbuf, int msglen) {
 
+int TCPRequestChannel::cwrite(void* msgbuf, int msglen) {
+    return write(sockfd, msgbuf, msglen);
 }
